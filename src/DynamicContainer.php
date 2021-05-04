6de207d2b5e0f3614eac511824f54544eeb4e2ac
Tomáš Kudělka <thorewi@gmail.com>
2021-05-04 16:32:54 +0200

    Update DynamicContainer.php

--- src/DynamicContainer.php
+++ src/DynamicContainer.php
@@ -39,6 +39,8 @@ class DynamicContainer extends BaseContainer
 
 	public function validate(?array $controls = NULL): void
 	{
+		parent::validate($controls);
+		
 		if (
 			$this->isRequired()
 			&&
