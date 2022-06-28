d199bfb8a0e00f0915f553fa0703d6012985a3a6
Radoslav Srp <106905019+msgrDiablo@users.noreply.github.com>
2022-06-28 21:41:42 +0200

    Update AnnotationsTrait.php

--- src/AnnotationsTrait.php
+++ src/AnnotationsTrait.php
@@ -2,10 +2,14 @@
 
 namespace ADT\Forms;
 
+use ADT\Components\AjaxSelect\AjaxMultiSelect;
+use ADT\Components\AjaxSelect\AjaxSelect;
+use ADT\Components\AjaxSelect\DynamicSelect;
 use ADT\Forms\Controls\CurrencyInput;
 use ADT\Forms\Controls\PhoneNumberInput;
 use ADT\Forms\DynamicContainer;
 use ADT\Forms\StaticContainer;
+use NasExt\Forms\Controls\DependentSelectBox;
 use Vodacek\Forms\Controls\DateInput;
 use ADT\EmailStrictInput;
 use \Closure;
@@ -17,6 +21,10 @@ use \Closure;
  * @method CurrencyInput addCurrency($name, $label = null, $currency = null)
  * @method StaticContainer addStaticContainer(string $name, Closure $factory, ?string $isFilledComponentName = null, ?string $isRequiredMessage = null)
  * @method DynamicContainer addDynamicContainer(string $name, Closure $factory, ?string $isFilledComponentName = null, ?string $isRequiredMessage = null)
+ * @method AjaxSelect addAjaxSelect($name, $label = null, $entityName = null, $entitySetupCallback = null, $config = [])
+ * @method AjaxMultiSelect addAjaxMultiSelect($name, $label = null, $entityName = null, $entitySetupCallback = null, $config = [])
+ * @method DependentSelectBox addDependentSelectBox($name, $label, $dependentControl1, $dependentControl2 = null)
+ * @method DynamicSelect addDynamicSelect($name, $label = NULL, $items = NULL, $itemFactory = NULL, $config = [])
  */
 trait AnnotationsTrait
 {
